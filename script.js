5955195653acdc62c83422dd55f01d867fd1bfac
jobayer-alam-24 <sheikhnayamul123@gmail.com>
2024-05-14 16:35:41 -0700

    Dom

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const mainCon = document.createElement('main');
 let boxCon = document.createElement('div');
 boxCon.classList.add('box-container');
+boxCon.style.padding = '30px';
 
-for (let i = 0; i <= 3; i++) {
+for (let i = 0; i <= 2; i++) {
     let box = document.createElement('div');
     let h3 = document.createElement('h3');
     let para = document.createElement('p');
 
     box.classList.add('box');
+    box.style.marginRight = '2%';
     
     h3.innerHTML = `BOX title`;
     para.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium odio, beatae distinctio ut aperiam eligendi vero voluptates alias ratione neque impedit odit deleniti architecto ducimus, tempora expedita dolore, quaerat pariatur.`;
